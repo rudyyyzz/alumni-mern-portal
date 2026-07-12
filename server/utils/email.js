ee61e5f03a848d0f9606028e36c283b8b68f7dc1
rudyyyzz <rudrastar2005@gmail.com>
2026-07-13 00:44:30 +0530

    email bug fixed

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,22 +1,23 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.EMAIL_FROM,
+  await transporter.sendMail({
+    from: `Alumni Portal <${process.env.EMAIL_FROM}>`,
     to: options.email,
     subject: options.subject,
     html: options.html,
-  };
-
-  await transporter.sendMail(mailOptions);
+  });
 };
 
-module.exports = sendEmail; 
+module.exports = sendEmail;
